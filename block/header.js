413a31c61f13a6d08d376022b9db3c1421a6bc13
ylshie <yuliang.hsieh@gmail.com>
2022-09-21 16:37:21 +0800

    fix layout issues

--- block/header.js
+++ block/header.js
@@ -6,8 +6,8 @@ import styles from './header.module.css';
 class Header extends React.Component {
     render() {
         return (
-            <div className={styles.header}>
-                <div id={styles.logo}>
+            <div className={styles.header}>                
+                <span id={styles.logo}>
                     <a href="/">
                         <Image
                             src= {"/images/LOGO.png"} 
@@ -15,44 +15,49 @@ class Header extends React.Component {
                             alt="LOGO"
                         />
                     </a>
-                </div>
-                <div id={styles.textCol} className={styles.headText}>
-                    <a href="/collection">
-                    Collection
-                    </a>
-                </div>
-                <div id={styles.textFAQ} className={styles.headText}>
-                    <a href="/faq">
-                    FAQ
-                    </a>  
-                </div>
-                <div id={styles.iconTwitter}>
-                    <a href="https://twitter.com/TRADE3_0">
-                        <Image  src= {"/images/Twitter.png"} // Route of the image file
-                                height={40} // Desired size with correct aspect ratio
-                                width={40} // Desired size with correct aspect ratio
-                                alt="Twitter"
-                        />
-                    </a>
-                </div>
-                <div id={styles.iconFB}>
-                    <a href="https://www.facebook.com/profile.php?id=100083408177430">
-                        <Image  src= {"/images/FB.png"} // Route of the image file
-                                height={30} // Desired size with correct aspect ratio
-                                width={30} // Desired size with correct aspect ratio
-                                alt="Facebook"
-                        />
-                    </a>
-                </div>
-                <div id={styles.iconDC}>
-                    <a href="https://discord.gg/gR67UnMVeK">
-                        <Image  src= {"/images/DC.png"} // Route of the image file
-                                height={20} // Desired size with correct aspect ratio
-                                width={20} // Desired size with correct aspect ratio
-                                alt="DC"
-                        />
-                    </a>
-                </div>
+                </span>
+                <span id={styles.appendtext}>
+                    <span id={styles.textCol} className={styles.headText}>
+                        <a href="/collection">
+                        Collection
+                        </a>
+                    </span>
+                    <span id={styles.textFAQ} className={styles.headText}>
+                        <a href="/faq">
+                        FAQ
+                        </a>  
+                    </span>
+                </span>
+                <span className={styles.iconbox}>
+                    <span id={styles.iconTwitter} className={styles.icon}>
+                        <a href="https://twitter.com/TRADE3_0">
+                            <Image  src= {"/images/Twitter.png"} // Route of the image file
+                                    height={30} // Desired size with correct aspect ratio
+                                    width={30} // Desired size with correct aspect ratio
+                                    alt="Twitter"
+                            />
+                        </a>
+                    </span>
+                    <span id={styles.iconFB} className={styles.icon}>
+                        <a href="https://www.facebook.com/profile.php?id=100083408177430">
+                            <Image  src= {"/images/FB.png"} // Route of the image file
+                                    height={30} // Desired size with correct aspect ratio
+                                    width={30} // Desired size with correct aspect ratio
+                                    alt="Facebook"
+                            />
+                        </a>
+                    </span>
+                    <span id={styles.iconDC} className={styles.icon}>
+                        <a href="https://discord.gg/gR67UnMVeK">
+                            <Image  src= {"/images/DC.png"} // Route of the image file
+                                    height={30} // Desired size with correct aspect ratio
+                                    width={30} // Desired size with correct aspect ratio
+                                    alt="DC"
+                            />
+                        </a>
+                    </span>
+                </span>
+                
                 {/*
                 <div id={styles.iconProfile}>
                     <a>
